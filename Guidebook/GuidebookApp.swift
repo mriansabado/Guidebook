5fab74cd15088248dcb134f71532d02fb903deee
Ian <isabado@onpointglobal.com>
2024-07-14 21:08:16 -0700

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Onpoint on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
